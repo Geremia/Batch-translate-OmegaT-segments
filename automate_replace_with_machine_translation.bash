4a644054bbb1ddf9914d3a2d9a295d8fb954e9cf
Geremia <Geremia@users.noreply.github.com>
2026-03-16 14:04:19 -0700

    added more delays to give it time to fetch the proper translation Previously, it seems it was going too fast and translations from translation memory were being inserted instead of machine translations.

--- automate_replace_with_machine_translation.bash
+++ automate_replace_with_machine_translation.bash
@@ -5,12 +5,15 @@
 # Make sure "Options" → "Machine Translation" → "Automatically Fetch Translations" is checked ☑; this speeds up translation.
 
 max_segments=${1:?max segments to translate}
-delay=${2:?delay in seconds between segments}
+delay=${2:?delay in seconds between actions}
 
 WID=$(xdotool search --name "OmegaT [0-9]")
 
 xdotool windowactivate --sync $WID
 for i in $(seq 1 $max_segments); do
+  sleep $delay
+  xdotool key ctrl+m
+  sleep $delay
   xdotool key ctrl+m
   sleep $delay
   xdotool key ctrl+u
